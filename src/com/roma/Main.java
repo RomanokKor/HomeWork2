1ff7f55bc5e8a233ee657b0c5be06a842cc41e65
RomanokKor <roma.kornilov.1995@mail.ru>
2021-09-26 17:59:39 +0300

    Initial commit

---
+++ src/com/roma/Main.java
@@ -0,0 +1,13 @@
+package com.roma;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a; //объявление переменной а типа double
+        a = 123.12431; //присвоение переменной а значения
+        byte b; //объявление переменной b типа byte
+        b = 127; //присвоение переменной b максимального значения
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
